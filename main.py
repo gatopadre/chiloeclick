9f307b224ec6de6f88a92d58b8d00f99a101113d
Seba Zuniga <sebastianzunigasaavedra@gmail.com>
2022-07-04 13:20:13 -0400

    subiendo avance

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', title='Home | Chiloe')
+    return render_template('index.html', title='Chiloeclick | Home - Turismo en Chiloe, Tours, Cabañas, Hotel')
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True, host='0.0.0.0')
